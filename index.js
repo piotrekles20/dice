800f3d1d3b4e440d68799b462e717af34c6678c7
piotrekles20 <88188402+piotrekles20@users.noreply.github.com>
2021-08-01 14:31:50 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+
+    var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+ 
+   var randomDiceImage = "dice"+ randomNumber1 +".png";
+   var randomImageSource="images/"+randomDiceImage;
+   var image1=document.querySelectorAll("img")[0];
+   image1.setAttribute("src", randomImageSource);
